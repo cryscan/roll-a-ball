7ed1ec23bd1914e054ce55f0ec01f0486f42bb19
cryscan <cryscan@umich.edu>
2020-09-03 16:48:49 -0400

    Camera following.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] GameManager game;
     [SerializeField] Camera playerCamera;
+    [SerializeField] float cameraDistance = 15;
+    [SerializeField] float cameraRadicalFalloff = 1;
+    [SerializeField] float cameraTangentialSpeed = 20;
 
     [SerializeField] float fallThreshold = -10;
     [SerializeField] float verticalTorque = 100;
@@ -35,8 +38,33 @@ public class PlayerController : MonoBehaviour
         if (transform.position.y < fallThreshold) game.Reset();
     }
 
+    void UpdateCamera()
+    {
+        var trans = playerCamera.transform;
+        var look = transform.position - trans.position;
+        trans.rotation = Quaternion.LookRotation(look, Vector3.up);
+
+        var balanceAngle = Vector3.Angle(Vector3.up, transform.right);
+        Debug.DrawRay(transform.position, transform.right);
+        if (balanceAngle < 10 || balanceAngle > 170) return;
+
+        var planeLook = new Vector2(look.x, look.z);
+        var velocity = new Vector2(rb.velocity.x, rb.velocity.z);
+        var distance = planeLook.magnitude;
+        var angle = Vector2.SignedAngle(velocity, planeLook);
+        if (distance > cameraDistance && Mathf.Abs(angle) < 90)
+        {
+            var lookDir = planeLook.normalized;
+            var radical = lookDir * (distance - cameraDistance);
+            var tangent = new Vector2(-lookDir.y, lookDir.x) * angle * Mathf.Deg2Rad * cameraTangentialSpeed;
+            var target = trans.position + new Vector3(radical.x, 0, radical.y) + new Vector3(tangent.x, 0, tangent.y);
+            trans.position = Vector3.Lerp(trans.position, target, 1 - Mathf.Exp(-cameraRadicalFalloff * Time.deltaTime));
+        }
+    }
+
     void Update()
     {
+        UpdateCamera();
         FallReset();
     }
 
