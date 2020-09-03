39a4e7cb1f27b86070c3bc279255c57494b44e4f
cryscan <cryscan@umich.edu>
2020-09-03 11:34:19 -0400

    Set up player.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public string levelName = "Level";
+
+    [SerializeField] Animator animator;
+
+    public void Reset()
+    {
+        StartCoroutine(ResetCoroutine());
+    }
+
+    IEnumerator ResetCoroutine()
+    {
+        animator.SetTrigger("fade");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour, InputActions.IPlayerActions
+{
+    [SerializeField] float verticalTorque;
+    [SerializeField] float horizontalTorque;
+
+    InputActions inputActions;
+
+    Rigidbody rb;
+    Vector2 movement;
+
+    void OnDisable()
+    {
+        inputActions.Player.Disable();
+    }
+
+    void Awake()
+    {
+        inputActions = new InputActions();
+        inputActions.Player.Enable();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        inputActions.Player.SetCallbacks(this);
+        Debug.Log("Started");
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movement = context.ReadValue<Vector2>();
+        Debug.Log($"Horizontal: {movement.x}, Vertical: {movement.y}");
+    }
+
+    public void OnFire(InputAction.CallbackContext context) { }
+
+    public void OnLook(InputAction.CallbackContext context) { }
+
+    void FixedUpdate()
+    {
+        float torque = verticalTorque;
+        var verticalAxis = transform.right * movement.y;
+        rb.AddTorque(torque * verticalAxis);
+
+        torque = horizontalTorque;
+        var horizontalAxis = -transform.forward * movement.x;
+        rb.AddTorque(torque * horizontalAxis);
+    }
+}
